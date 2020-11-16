d909b1605611a27c921cfc9a6a685bc6c46096fb
trimardianto27 <74308784+trimardianto27@users.noreply.github.com>
2020-11-16 21:53:28 +0700

    Add files via upload

---
+++ Tugas bahasa C.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+  int A,B,jumlah,kuadrat;
+  printf("Masukkan angka :");
+  scanf("%d", &A);
+  printf("Masukkan angka :");
+  scanf("%d", &B);
+  jumlah=A+B;
+  kali=jumlah*jumlah;
+  printf("hasil (%d * %d)^2 = %d",A,B,kali);
+}
